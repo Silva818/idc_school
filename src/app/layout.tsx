fef8964e29fdcde284237276a9c7fb6e073cf40d
Hovhannes <shat@MacBook-Air-Hovhannes.local>
2026-01-21 09:22:14 +0100

    Ваше описание изменений

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 // src/app/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -22,7 +23,6 @@ export const metadata: Metadata = {
     "Bodyweight training in a comfortable pace with a focus on technique.",
 };
 
-
 export default function RootLayout({
   children,
 }: {
@@ -41,6 +41,34 @@ export default function RootLayout({
           overflow-x-hidden
         `}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W79W5FMR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W79W5FMR');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {children}
       </body>
     </html>
